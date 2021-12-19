d8f46526c4a22744e5badcf6d682d45a40960ca0
toxi42 <79251578+toxi42@users.noreply.github.com>
2021-12-19 04:30:43 +0300

    Update autostaking.sh

--- autostaking.sh
+++ autostaking.sh
@@ -9,31 +9,33 @@ DELAY=3600/4 #in secs - how often restart the script
 ACC_NAME= #example: = ACC_NAME=wallet30
 NODE="tcp://localhost:26657" #change it only if you use another rpc port of your node
 CHAIN_NAME=evmos_9000-2
+project=evmosd
+coin=aphoton
 for (( ;; )); do
         echo -e "Get reward from Delegation"
-        echo -e "${PWD}\ny\n" | evmosd tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id ${CHAIN_NAME} --from ${DELEGATOR_ADDRESS} --gas=auto --fees=1000aphoton --commission --node ${NODE} --yes
+        echo -e "${PWD}\ny\n" | ${project} tx distribution withdraw-rewards ${VALIDATOR_ADDRESS} --chain-id ${CHAIN_NAME} --from ${DELEGATOR_ADDRESS} --gas=auto --fees=1000${coin} --commission --node ${NODE} --yes
 for (( timer=30; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOUT_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=$(evmosd q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount')
-echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} aphoton\n"
+BAL=$(${project} q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount')
+echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${coin}\n"
         echo -e "Claim rewards\n"
-        echo -e "${PWD}\n${PWD}\n" | evmosd tx distribution withdraw-all-rewards --from ${DELEGATOR_ADDRESS} --chain-id ${CHAIN_NAME} --gas=auto --fees=1000aphoton --node ${NODE} --yes
+        echo -e "${PWD}\n${PWD}\n" | ${project} tx distribution withdraw-all-rewards --from ${DELEGATOR_ADDRESS} --chain-id ${CHAIN_NAME} --gas=auto --fees=1000${coin} --node ${NODE} --yes
 for (( timer=30; timer>0; timer-- ))
         do
                 printf "* sleep for ${RED_COLOR}%02d${WITHOU_COLOR} sec\r" $timer
                 sleep 1
         done
-BAL=$(evmosd q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
+BAL=$(${project} q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
         BAL=$(($BAL-1000000))
-echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} aphoton\n"
+echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${coin}\n"
         echo -e "Stake ALL\n"
 if (( BAL > 1000000 )); then
-            echo -e "${PWD}\n${PWD}\n" | evmosd tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}aphoton --from ${DELEGATOR_ADDRESS} --gas=auto --fees=1000aphoton --node ${NODE} --chain-id ${CHAIN_NAME}  --yes
+            echo -e "${PWD}\n${PWD}\n" | ${project} tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}${coin} --from ${DELEGATOR_ADDRESS} --gas=auto --fees=1000${coin} --node ${NODE} --chain-id ${CHAIN_NAME}  --yes
         else
-                                echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} aphoton BAL < 10000000 ((((\n"
+                                echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} ${coin} BAL < 10000000 ((((\n"
         fi
 for (( timer=${DELAY}; timer>0; timer-- ))
         do
